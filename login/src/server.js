6bfac85c63e4ab4f4ae7d82a575ff3132fbd6bf1
Jose Antonio Marañón Calvario <65850464+Josemc21@users.noreply.github.com>
2020-12-06 21:08:08 +0100

    Chat y datos de registro e inicio de sesion añadidos

--- login/src/server.js
+++ login/src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const app2 = express();
 const path = require("path");
 const fetch = require('node-fetch');
 const mongoose = require("mongoose");
@@ -10,6 +11,8 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const { stringify } = require('querystring');
+let randomColor = require('randomcolor');
+const uuid = require('uuid'); 
 
 const {url} = require("./config/database");
 
@@ -24,6 +27,17 @@ app.set("view engine", "ejs");
 
 //middleware
 app.use(morgan("dev"));
+// Add headers
+/*app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  next();
+});*/
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({
@@ -71,3 +85,98 @@ app.post('/subscribe', async (req, res) => {
   // If successful
   return res.json({ success: true, msg: 'Captcha passed' });
 });
+
+//Disable x-powered-by header
+app2.disable('x-powered-by')
+
+//middlewares
+app2.use(express.static('public'));
+
+// Add headers
+/*app2.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Pass to next layer of middleware
+  next();
+});*/
+
+//routes
+app2.get('/chat', (req,res)=>{
+    res.sendFile(__dirname + '/views/chat.ejs');
+});
+
+//Listen on port 5000
+server = app2.listen( process.env.PORT || 5000, () => {
+  console.log("server on port", 5000);
+});
+
+//socket.io instantiation
+const io = require("socket.io")(server,{
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+let users = [];
+let connnections = [];
+
+//listen on every connection
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  connnections.push(socket)
+  //initialize a random color for the socket
+  let color = randomColor();
+
+  socket.username = 'Anonymous';
+  socket.color = color;
+
+  //listen on change_username
+  socket.on('change_username', data => {
+      let id = uuid.v4(); // create a random id for the user
+      socket.id = id;
+      socket.username = data.nickName;
+      users.push({id, username: socket.username, color: socket.color});
+      updateUsernames();
+  })
+
+  //update Usernames in the client
+  const updateUsernames = () => {
+      io.sockets.emit('get users',users)
+  }
+
+  //listen on new_message
+  socket.on('new_message', (data) => {
+      //broadcast the new message
+      io.sockets.emit('new_message', {message : data.message, username : socket.username,color: socket.color});
+  })
+
+  //listen on typing
+  socket.on('typing', data => {
+      socket.broadcast.emit('typing',{username: socket.username})
+  })
+
+  //Disconnect
+  socket.on('disconnect', data => {
+
+      if(!socket.username)
+          return;
+      //find the user and delete from the users list
+      let user = undefined;
+      for(let i= 0;i<users.length;i++){
+          if(users[i].id === socket.id){
+              user = users[i];
+              break;
+          }
+      }
+      users = users.filter( x => x !== user);
+      //Update the users list
+      updateUsernames();
+      connnections.splice(connnections.indexOf(socket),1);
+  })
+})
